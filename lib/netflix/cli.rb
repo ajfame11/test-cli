4b932ecf7752aa1105196210b6cce7a7fb7c145e
aurel fontaine <ajfame11@gmail.com>
2020-05-06 02:37:16 +0000

    Continued with problem

--- lib/netflix/cli.rb
+++ lib/netflix/cli.rb
@@ -1,8 +1,16 @@
 class Cli
   def run
     puts " "
-    puts "Hello and welcome to Netflix Top10"
+    puts "Hello and welcome to Netflix Top10!"
     puts " "
+    puts "Please choose a genre."
+    puts "1. action"
+    puts "2. adventure"
+    puts "3. comedy"
+    puts "4. horror"
+    puts "5. romance"
+    puts " "
+    @genre = gets.strip
   end
 end
 
