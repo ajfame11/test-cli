bc10e5bdd3ea95196fd47349abfa0100f4e14919
aurel fontaine <ajfame11@gmail.com>
2020-05-06 02:28:23 +0000

    First problem with cli folder

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,1 +1,10 @@
 ### list dependencies for project
+
+require "pry"
+require "httparty"  ### Interacts with the api
+require "json"   ### Parsing data from api in json
+
+
+require_relative "./netflix/cli"
+require_relative "./netflix/api"
+require_relative "./netflix/show"
--- lib/netflix/cli.rb
+++ lib/netflix/cli.rb
@@ -1,7 +1,9 @@
 class Cli
-  
-  
-  
+  def run
+    puts " "
+    puts "Hello and welcome to Netflix Top10"
+    puts " "
+  end
 end
 
 ### Handle INPUT from User and OUTPUT to User
